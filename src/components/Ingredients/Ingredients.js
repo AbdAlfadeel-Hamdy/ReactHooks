15df81a03c44fa86b1fc7c3ddfebf29a971412a1
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-11-12 05:39:31 +0200

    useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,25 +1,61 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList";
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
-  const addIngredientHandler = (ingredient) =>
+
+  // useEffect(() => {
+  //   const loadIngredients = async () => {
+  //     const response = await fetch(
+  //       `https://ingredients-df31b-default-rtdb.firebaseio.com/ingredients.json`
+  //     );
+  //     const data = await response.json();
+  //     console.log(data);
+  //     const loadedIngredients = [];
+  //     for (const key in data)
+  //       loadedIngredients.push({
+  //         id: key,
+  //         title: data[key].title,
+  //         amount: data[key].amount,
+  //       });
+  //     setIngredients(loadedIngredients);
+  //   };
+  //   loadIngredients();
+  // }, []);
+
+  const addIngredientHandler = async (ingredient) => {
+    const response = await fetch(
+      `https://ingredients-df31b-default-rtdb.firebaseio.com/ingredients.json`,
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
     setIngredients((prevIngredients) => [
       ...prevIngredients,
       {
-        id: Math.random().toString(),
+        id: data.name,
         title: ingredient.title,
         amount: ingredient.amount,
       },
     ]);
+  };
+
+  const filterIngredientsHandler = useCallback(
+    (ingredients) => setIngredients(ingredients),
+    []
+  );
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
-
       <section>
-        <Search />
+        <Search onFilterIngredients={filterIngredientsHandler} />
         <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
       </section>
     </div>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
-const Search = React.memo(props => {
+const Search = React.memo(({ onFilterIngredients }) => {
+  const [searchInput, setSearchInput] = useState("");
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const loadIngredients = async () => {
+        const response =
+          await fetch(`https://ingredients-df31b-default-rtdb.firebaseio.com/ingredients.json
+        `);
+        const data = await response.json();
+        const loadedIngredients = [];
+        for (const key in data) {
+          loadedIngredients.push({
+            id: key,
+            title: data[key].title,
+            amount: data[key].amount,
+          });
+        }
+        const filteredIngredients = loadedIngredients.filter((ing) =>
+          ing.title.startsWith(searchInput)
+        );
+        if (!searchInput) return onFilterIngredients(loadedIngredients);
+        onFilterIngredients(filteredIngredients);
+      };
+      loadIngredients();
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [searchInput, onFilterIngredients]);
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
         </div>
       </Card>
     </section>
