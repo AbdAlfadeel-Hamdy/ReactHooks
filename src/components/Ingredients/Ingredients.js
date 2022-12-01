632419c829988b3ea34e8876eae41ce4966b0f88
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-12-01 03:01:22 +0200

    Error handling

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,8 +3,11 @@ import React, { useCallback, useState } from "react";
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   // useEffect(() => {
   //   const loadIngredients = async () => {
@@ -26,37 +29,72 @@ function Ingredients() {
   // }, []);
 
   const addIngredientHandler = async (ingredient) => {
-    const response = await fetch(
-      `https://ingredients-df31b-default-rtdb.firebaseio.com/ingredients.json`,
-      {
-        method: "POST",
-        body: JSON.stringify(ingredient),
-        headers: {
-          "Content-Type": "application/json",
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://ingredients-df31b-default-rtdb.firebaseio.com/ingredients.json`,
+        {
+          method: "POST",
+          body: JSON.stringify(ingredient),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setIsLoading(false);
+      const data = await response.json();
+      setIngredients((prevIngredients) => [
+        ...prevIngredients,
+        {
+          id: data.name,
+          title: ingredient.title,
+          amount: ingredient.amount,
         },
-      }
-    );
-    const data = await response.json();
-    setIngredients((prevIngredients) => [
-      ...prevIngredients,
-      {
-        id: data.name,
-        title: ingredient.title,
-        amount: ingredient.amount,
-      },
-    ]);
+      ]);
+    } catch (error) {
+      setError(`Something went wrong!`);
+      setIsLoading(false);
+    }
   };
 
   const filterIngredientsHandler = useCallback(
     (ingredients) => setIngredients(ingredients),
     []
   );
+  const removeItemHandler = async (itemId) => {
+    try {
+      setIsLoading(true);
+      await fetch(
+        `https://ingredients-df31b-default-rtdb.firebaseio.com/ingredients/${itemId}.json`,
+        {
+          method: "DELETE",
+        }
+      );
+      setIsLoading(false);
+      setIngredients((prevIngredients) =>
+        prevIngredients.filter((ing) => ing.id !== itemId)
+      );
+    } catch (error) {
+      setError(`Something went wrong!`);
+      setIsLoading(false);
+    }
+  };
+  const clearError = () => {
+    setError(null);
+  };
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
       <section>
         <Search onFilterIngredients={filterIngredientsHandler} />
-        <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
+        <IngredientList
+          ingredients={ingredients}
+          onRemoveItem={removeItemHandler}
+        />
       </section>
     </div>
   );
